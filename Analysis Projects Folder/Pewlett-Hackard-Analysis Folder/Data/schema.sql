4d2d62c0c471865e4cfaf38b525df464d107bdfc
alfrescas <107371010+alfrescas@users.noreply.github.com>
2022-10-03 14:34:21 -0600

    Adding schema

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/schema.sql
@@ -1,23 +1,47 @@
--- Drop table if exists
-DROP TABLE firepower;
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
 
--- Create new table to import data
-CREATE TABLE firepower (
-	country VARCHAR,
-	ISO3 VARCHAR,
-	rank INT,
-	TotalPopulation INT,
-	ManpowerAvailable INT,
-	TotalMilitaryPersonnel INT,
-	ActivePersonnel INT,
-	ReservePersonnel INT,
-	TotalAircraftStrength INT,
-	FighterAircraft INT,
-	AttackAircraft INT,
-	TotalHelicopterStrength INT,
-	AttackHelicopters INT
 );
 
--- Import data from firepower.csv
--- View the table to ensure all data has been imported correctly
-SELECT * FROM firepower;
+CREATE TABLE employees (
+emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+SELECT * FROM departments;
