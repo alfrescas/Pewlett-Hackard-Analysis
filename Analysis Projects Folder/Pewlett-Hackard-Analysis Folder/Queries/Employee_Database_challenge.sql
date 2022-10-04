709e3f97b1293668ba67cf41b1f60b442c21bfa5
alfrescas <107371010+alfrescas@users.noreply.github.com>
2022-10-03 22:03:19 -0600

    Updated sql code

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -1,10 +1,37 @@
 SELECT e.emp_no, first_name, last_name,
        title, from_date, to_date
+	   into retirement_titles
 FROM employees as e
 Inner Join titles as t
 on e.emp_no = t.emp_no
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
+
+SELECT distinct on (emp_no)emp_no, first_name, last_name,title
+into unique_titles
+from retirement_titles	  
+Where to_date = '9999-01-01'
+Order by emp_no
+
+Select count (title), title
+into retiring_titles
+from unique_titles
+group by title
+order by  count (title) desc
+
+
+SELECT distinct on (e.emp_no)e.emp_no, first_name, last_name, 
+de.from_date, de.to_date,
+title
+into mentorship_eligibilty
+from employees as e
+join dept_emp as de
+on e.emp_no = de.emp_no
+join titles as t
+on e.emp_no = t.emp_no
+Where de.to_date = '9999-01-01'
+and (birth_date between '1965-01-01'and '1965-12-31')
+Order by e.emp_no
 
 Select * from salaries
 
@@ -23,7 +50,7 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
-CREATE TABLE dept_emp (
+CREATE TABLE department_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
@@ -45,3 +72,12 @@ FROM pg_catalog.pg_tables
 WHERE schemaname != 'pg_catalog' AND 
     schemaname != 'information_schema';
 
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
